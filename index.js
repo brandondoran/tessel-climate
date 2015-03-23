70c86e211cd9c0845a392d83fd322a1c9c64c239
Brandon Doran <bdoran@gmail.com>
2015-03-22 21:40:24 -0700

    read climate sensor, post results to keen.io

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var tessel = require('tessel');
 var climatelib = require('climate-si7020');
 var wifi = require('wifi-cc3000');
 var async = require('async');
+var Keen = require('keen-js');
 
 var climate = climatelib.use(tessel.port.A);
+var keen = new Keen({
+  writeKey: '565ef9dbdde2f0039758c2b41222228be7ee65a1c3251c7068b5c7dca9dd8ef21b906fdd6d65f1bc8658ca85f87e769c939440a8ee4876016d0ce0679503ccebbe14c5f4e1e33d748fd2c73ac8da2a91ed17cb24179d6805617d3dab38d271bd7a95eac83acdfc4f4142e952a09bcd77',
+  projectId: '550ba184c1e0ab2052426f3a'
+});
 
 function readSensor (callback) {
   async.series({
@@ -22,9 +27,21 @@ function readSensor (callback) {
   });
 }
 
+function postData (data, callback) {
+  keen.addEvent('climate', data, callback);
+  //callback(null, data);
+}
+
 climate.on('ready', function() {
   console.log('climate sensor ready');
-  readSensor(function(err, result) {
+  async.auto({
+    request: function(done) {
+      readSensor(done);
+    },
+    response: ['request', function(done, results) {
+      postData(results.request, done);
+    }]
+  }, function(err, result) {
     if (err) {
       console.error(err);
     } else {
